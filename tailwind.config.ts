58c87f843c98ab724ffe4848aaa378d145143a26
reddiedev <red.mendoza@shopee.com>
2023-09-25 00:04:31 +0800

    added custom glow

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,14 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {},
-  plugins: [],
+    content: ["./src/pages/**/*.{js,ts,jsx,tsx,mdx}", "./src/components/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
+    theme: {
+        extend: {
+            dropShadow: {
+                glow: ["0 0px 3px rgba(255,255, 255, 0.8)", "0 0px 10px rgba(255, 255,255, 0.3)"]
+            }
+        }
+    },
+    plugins: []
 };
 export default config;
