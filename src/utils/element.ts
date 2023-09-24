1665bb7923e49c9bdf0ecffc4d01de1a68728469
reddiedev <red.mendoza@shopee.com>
2023-09-24 23:23:55 +0800

    added remaining elements

--- src/utils/element.ts
+++ src/utils/element.ts
@@ -12,6 +12,62 @@ export function isElementIs({ xIndex, yIndex }: { xIndex: number; yIndex: number
     return true;
 }
 
+export function isElementTwenty({ xIndex, yIndex }: { xIndex: number; yIndex: number }): boolean {
+    if (yIndex > 0) return false;
+    if (xIndex < 7) return false;
+
+    return true;
+}
+
+export function isElementQuarter({ xIndex, yIndex }: { xIndex: number; yIndex: number }): boolean {
+    if (yIndex !== 1) return false;
+    if (xIndex > 6) return false;
+
+    return true;
+}
+
+export function isElementHalf({ xIndex, yIndex }: { xIndex: number; yIndex: number }): boolean {
+    if (yIndex !== 1) return false;
+    if (xIndex < 8) return false;
+
+    return true;
+}
+
+export function isElementTenPref({ xIndex, yIndex }: { xIndex: number; yIndex: number }): boolean {
+    if (yIndex !== 2) return false;
+    if (xIndex > 2) return false;
+
+    return true;
+}
+
+export function isElementFivePref({ xIndex, yIndex }: { xIndex: number; yIndex: number }): boolean {
+    if (yIndex !== 2) return false;
+    if (xIndex < 3 || xIndex > 6) return false;
+
+    return true;
+}
+
+export function isElementPast({ xIndex, yIndex }: { xIndex: number; yIndex: number }): boolean {
+    if (yIndex !== 2) return false;
+    if (xIndex < 8) return false;
+
+    return true;
+}
+
+export function isElementTo({ xIndex, yIndex }: { xIndex: number; yIndex: number }): boolean {
+    if (yIndex !== 3) return false;
+    if (xIndex > 1) return false;
+
+    return true;
+}
+
+export function isElementTwelve({ xIndex, yIndex }: { xIndex: number; yIndex: number }): boolean {
+    if (yIndex !== 3) return false;
+    if (xIndex < 3 || xIndex > 7) return false;
+
+    return true;
+}
+
 export function isElementOne({ xIndex, yIndex }: { xIndex: number; yIndex: number }): boolean {
     if (yIndex !== 3) return false;
     if (xIndex < 10) return false;
@@ -57,6 +113,41 @@ export function isElementSix({ xIndex, yIndex }: { xIndex: number; yIndex: numbe
 export function isElementSeven({ xIndex, yIndex }: { xIndex: number; yIndex: number }): boolean {
     if (yIndex !== 5) return false;
     if (xIndex < 7) return false;
+
+    return true;
+}
+
+export function isElementEight({ xIndex, yIndex }: { xIndex: number; yIndex: number }): boolean {
+    if (yIndex !== 6) return false;
+    if (xIndex > 4) return false;
+
+    return true;
+}
+
+export function isElementNine({ xIndex, yIndex }: { xIndex: number; yIndex: number }): boolean {
+    if (yIndex !== 6) return false;
+    if (xIndex < 5 || xIndex > 8) return false;
+
+    return true;
+}
+
+export function isElementTen({ xIndex, yIndex }: { xIndex: number; yIndex: number }): boolean {
+    if (yIndex !== 6) return false;
+    if (xIndex < 9) return false;
+
+    return true;
+}
+
+export function isElementEleven({ xIndex, yIndex }: { xIndex: number; yIndex: number }): boolean {
+    if (yIndex !== 7) return false;
+    if (xIndex < 3 || xIndex > 8) return false;
+
+    return true;
+}
+
+export function isElementOclock({ xIndex, yIndex }: { xIndex: number; yIndex: number }): boolean {
+    if (yIndex !== 8) return false;
+    if (xIndex == 0 || xIndex > 7) return false;
 
     return true;
 }
